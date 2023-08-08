0daade1f795ce12a29f3b3a830e1b4c517e47025
chuparts <asylzhan2606@gmail.com>
2023-08-08 09:45:45 +0600

    Implemented open container (now only 2 classes)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:animations/animations.dart';
 
 late Database db;
+var title = "Asyl Notes";
 
 void main() async {
   GoogleFonts.config.allowRuntimeFetching = false; //disabling HTTP requests
@@ -21,15 +23,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Asyl Notes',
-      theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
-        fontFamily: 'JosefinSans',
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.lightGreen,
+          fontFamily: 'JosefinSans',
+        ),
+        debugShowCheckedModeBanner: false,
+        scrollBehavior: const ConstantScrollBehavior(),
+        home: const MyHomePage(title: 'Asyl Notes'),
       ),
-      debugShowCheckedModeBanner: false,
-      scrollBehavior: const ConstantScrollBehavior(),
-      home: const MyHomePage(title: 'Asyl Notes'),
     );
   }
 }
@@ -47,30 +52,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // var appState = context.watch<MyAppState>();
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
-          leading: currentNoteId < 0
-              ? const Icon(Icons.home) //TODO: Create logo and add here
-              : IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: () => setState(() {
-                    currentNoteId = -1;
-                  }),
-                ),
-          actions: [
-            currentNoteId < 0
-                ? const SizedBox()
-                : IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () async {
-                      await db.delete("notes", where: "id = $currentNoteId");
-                      setState(() {
-                        currentNoteId = -1;
-                      });
-                    },
-                  ),
-          ],
+          leading: const Icon(Icons.home),
+          //     ? const Icon(Icons.home) //TODO: Create logo and add here
+          //     : IconButton(
+          //         icon: const Icon(Icons.arrow_back),
+          //         onPressed: () => setState(() {
+          //           currentNoteId = -1;
+          //         }),
+          //       ),
+          // actions: [
+          //   currentNoteId < 0
+          //       ? const SizedBox()
+          //       : IconButton(
+          //           icon: const Icon(Icons.delete),
+          //           onPressed: () async {
+          //             await db.delete("notes", where: "id = $currentNoteId");
+          //             setState(() {
+          //               currentNoteId = -1;
+          //             });
+          //           },
+          //         ),
+          // ],
         ),
         floatingActionButton: currentNoteId >= 0
             ? null
@@ -93,31 +99,77 @@ class _MyHomePageState extends State<MyHomePage> {
                 (BuildContext context2, AsyncSnapshot<List<Map>> snapshot) {
               List<Note> notes = [];
               if (!snapshot.hasData) {
-                return Text("Loading...");
+                return const Text("Loading...", style: TextStyle(fontSize: 20));
               }
               for (Map m in snapshot.data!) {
                 notes.add(Note(m["id"], m["title"], m["note_text"]));
               }
               if (notes.isEmpty) {
-                return EmptyNotesPage();
+                return const EmptyNotesPage();
               }
-              if (currentNoteId < 0) {
-                return NoteListPage(
-                    noteList: notes,
-                    onNoteSelected: (index) => setState(() {
-                          currentNoteId = notes[index].id;
-                        }));
-              } else {
-                for (Note n in notes) {
-                  if (n.id == currentNoteId) {
-                    return EditorPage(n);
-                  }
-                }
-                return Text("Note not found");
-              }
+                return GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 200,
+                            childAspectRatio: 3 / 2,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10),
+                    itemCount: notes.length,
+                    itemBuilder: (BuildContext ctx, index) {
+                      return OpenContainer(
+                        openBuilder: (context, closeContainer) {
+                          return EditorPage(notes[index]);
+                        },
+                        closedShape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        closedBuilder: (context, openContainer) {
+                          return InkWell(
+                            onTap: () {
+                              openContainer();
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    notes[index].title.isEmpty
+                                        ? "No title"
+                                        : notes[index].title,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  Text(
+                                    notes[index].text.isEmpty
+                                        ? ""
+                                        : notes[index].text,
+                                    maxLines: 3,
+                                    style:
+                                        const TextStyle(color: Colors.black38),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    });
             },
           ),
         ));
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  void update()
+  {
+    notifyListeners();
   }
 }
 
@@ -135,102 +187,69 @@ class EmptyNotesPage extends StatelessWidget {
   }
 }
 
-class NoteListPage extends StatelessWidget {
-  const NoteListPage(
-      {super.key, required this.noteList, required this.onNoteSelected});
-
-  final List<Note> noteList;
-  final Function(int) onNoteSelected;
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-        shrinkWrap: true,
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10),
-        itemCount: noteList.length,
-        itemBuilder: (BuildContext ctx, index) {
-          return OpenContainer(
-            openBuilder: (context, closedContainer) {return EditorPage(noteList[index]);},
-            closedShape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-            closedBuilder: (context, openContainer){return InkWell(
-              onTap: () {
-                //onNoteSelected.call(index);
-                openContainer();
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      noteList[index].title.isEmpty
-                          ? "No title"
-                          : noteList[index].title,
-                      maxLines: 1,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      noteList[index].text.isEmpty
-                          ? ""
-                          : noteList[index].text,
-                      maxLines: 3,
-                      style: const TextStyle(color: Colors.black38),
-                    ),
-                  ],
-                ),
-              ),
-            );},
-          );
-        });
-  }
-}
-
 class EditorPage extends StatelessWidget {
   final Note note;
   const EditorPage(this.note, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          maxLength: 45,
-          controller: TextEditingController(text: note.title),
-          decoration: const InputDecoration(
-              hintText: "Title", border: OutlineInputBorder()),
-          onChanged: (value) {
-            db.update("notes", {"title": value}, where: "id = ${note.id}");
+    var appState = context.watch<MyAppState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+            appState.update();
           },
-          //TODO: create thicker border and bold text
         ),
-        const SizedBox(
-          height: 8,
+        actions: [
+          IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () async {
+                      await db.delete("notes", where: "id = ${note.id}");
+                      appState.update();
+                      Navigator.pop(context);
+                    },
+                  ),
+          ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              maxLength: 45,
+              controller: TextEditingController(text: note.title),
+              decoration: const InputDecoration(
+                  hintText: "Title", border: OutlineInputBorder()),
+              onChanged: (value) {
+                db.update("notes", {"title": value}, where: "id = ${note.id}");
+              },
+              //TODO: create thicker border and bold text
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Expanded(
+              child: TextField(
+                maxLength: 2000,
+                textAlignVertical: TextAlignVertical.top,
+                expands: true,
+                maxLines: null,
+                controller: TextEditingController(text: note.text),
+                decoration: const InputDecoration(
+                    hintText: "Text", border: OutlineInputBorder()),
+                onChanged: (value) {
+                  db.update("notes", {"note_text": value},
+                      where: "id = ${note.id}");
+                },
+              ),
+            ),
+          ],
         ),
-        Expanded(
-          child: TextField(
-            maxLength: 2000,
-            textAlignVertical: TextAlignVertical.top,
-            expands: true,
-            maxLines: null,
-            controller: TextEditingController(text: note.text),
-            decoration: const InputDecoration(
-                hintText: "Text", border: OutlineInputBorder()),
-            onChanged: (value) {
-              db.update("notes", {"note_text": value},
-                  where: "id = ${note.id}");
-            },
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
